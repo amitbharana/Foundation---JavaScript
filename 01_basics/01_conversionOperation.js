adfb98157546f37850e9e90ec4bba4ff01d5e3e6
Amit kumar <amityadav6202671@gmail.com>
2026-03-20 20:48:40 +0000

    operations

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -1,28 +1,65 @@
-let age = "21"
+// let age = "21"
 
-console.log(typeof age);
-console.log(typeof(age));
+// console.log(typeof age);
+// console.log(typeof(age));
 
-let valueInNumber = Number(age)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
-// "33" => 33
-// "33anc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33anc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = "amit"
+// let isLoggedIn = "amit"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
+// // 1 => true; 0 => false
 
-// " " => false
-//"Amit" => true
+// // " " => false
+// //"Amit" => true
 
-let someNumber = 33
-let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+// ************** 0perations ************ //
+
+// let value = 3
+// let negValue = -value
+// //console.log(negValue);
+
+// let str1 =  "Amit"
+// let str2 = " Amains"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 2) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+
+++gameCounter;
+
+console.log(gameCounter);
