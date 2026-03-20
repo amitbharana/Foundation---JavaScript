3ec1197ac188c0460fca675ad1a564849184c1c8
Amit kumar <amityadav6202671@gmail.com>
2026-03-20 19:28:31 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,28 @@
+let age = "21"
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" => 33
+// "33anc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "amit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+
+// " " => false
+//"Amit" => true
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
