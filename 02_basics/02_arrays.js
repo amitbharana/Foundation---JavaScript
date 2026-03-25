2bff6a13df7c98428ff266950532a5050bcfcb6a
Amit kumar <amityadav6202671@gmail.com>
2026-03-25 19:30:59 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,32 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+console.log(real_another_array);
+
+// const Arrays = "amit"
+console.log(Array.isArray("amit"));
+
+console.log(Array.from("amit"));
+
+console.log(Array.from( {name: "Amit"})); //intesting 
+
+let score1 = 1000
+let score2 = 2000
+let score3 = 3000
+
+console.log(Array.of(score1,score2,score3));
